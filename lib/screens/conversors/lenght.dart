ed1837314b3b831123661e7934bf0356e968e686
chris.am <c.aranguizm@outlook.com>
2021-12-27 18:41:01 -0300

    showing dropdown button

--- lib/screens/conversors/lenght.dart
+++ lib/screens/conversors/lenght.dart
@@ -1,16 +1,56 @@
 import 'package:flutter/material.dart';
 
-class LenghtScreen extends StatelessWidget {
-  final Color color;
-  const LenghtScreen({required this.color});
+class LenghtScreen extends StatefulWidget {
+  const LenghtScreen({Key? key}) : super(key: key);
 
   @override
+  State<LenghtScreen> createState() => _LenghtScreenState();
+}
+
+class _LenghtScreenState extends State<LenghtScreen> {
+  //* this variable holds the value of the selected DropdownButton
+  String dropdownValue = 'Elije una opción';
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: color,
-      child: const Center(
-        child: Text('Length screen'),
-      ),
+    return Column(
+      children: [
+        //* Message
+        const Text(
+          'Elige la unidad a la que quieres convertir',
+          style: TextStyle(fontSize: 20),
+        ),
+        Center(
+          //* Creating Dropdown
+          child: DropdownButton(
+              hint: Text(dropdownValue),
+              //* List of items to convert
+              items: <String>[
+                "mil",
+                "pulgadas",
+                "Pie",
+                "Yarda",
+                "Rood",
+                "Cadena",
+                "Furlong",
+                "Milla",
+                "Legua",
+                "Link",
+                "Braza",
+              ].map<DropdownMenuItem<String>>(
+                (String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                },
+              ).toList(),
+              onChanged: (String? valueIn) {
+                setState(() {
+                  dropdownValue = valueIn!;
+                });
+              }),
+        ),
+      ],
     );
   }
 }
