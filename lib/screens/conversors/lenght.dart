4604cad9c74ea3e6ff79dc75ee7fbb322b1044ba
chris.am <c.aranguizm@outlook.com>
2021-12-31 01:46:11 -0300

    sing keyboard actions to give ios keyboard a done button

--- lib/screens/conversors/lenght.dart
+++ lib/screens/conversors/lenght.dart
@@ -12,88 +12,102 @@ class _LenghtScreenState extends State<LenghtScreen> {
   //* this variable holds the value of the selected DropdownButton
   String _currentOption = 'Elije una opción';
   String _inputValue = '';
+  FocusNode _focusInput = FocusNode();
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        //*Input Field container
-        SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
-            ),
-            child: Column(
-              children: [
-                //* Input field
-                TextFormField(
-                  keyboardType:
-                      const TextInputType.numberWithOptions(decimal: true),
-                  onChanged: (inputValue) {
-                    setState(() {
-                      _inputValue = inputValue;
-                    });
-                    print('now I have as _inputValue ==> $inputValue');
-                  },
-                  validator: (inputValue) {
-                    try {
-                      if (inputValue == null) return 'Este campo es requerido';
-                      return double.parse(inputValue) > 0
-                          ? null
-                          : 'El valor debe ser mayor a 0';
-                    } catch (e) {
-                      return 'El valor debe ser un número';
-                    }
-                  },
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  decoration: InputDecoration(
-                    hintText: _currentOption == 'Elije una opción'
-                        ? 'Selecciona una opción de la lista'
-                        : 'Ingresa el valor en $_currentOption',
+    return KeyboardActions(
+      config: KeyboardActionsConfig(
+        keyboardActionsPlatform: KeyboardActionsPlatform.IOS,
+        nextFocus: false,
+        actions: [
+          KeyboardActionsItem(
+            focusNode: _focusInput,
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          //*Input Field container
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              child: Column(
+                children: [
+                  //* Input field
+                  TextFormField(
+                    focusNode: _focusInput,
+                    keyboardType:
+                        const TextInputType.numberWithOptions(decimal: true),
+                    onChanged: (inputValue) {
+                      setState(() {
+                        _inputValue = inputValue;
+                      });
+                      print('now I have as _inputValue ==> $inputValue');
+                    },
+                    validator: (inputValue) {
+                      try {
+                        if (inputValue == null)
+                          return 'Este campo es requerido';
+                        return double.parse(inputValue) > 0
+                            ? null
+                            : 'El valor debe ser mayor a 0';
+                      } catch (e) {
+                        return 'El valor debe ser un número';
+                      }
+                    },
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: InputDecoration(
+                      hintText: _currentOption == 'Elije una opción'
+                          ? 'Selecciona una opción de la lista'
+                          : 'Ingresa el valor en $_currentOption',
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
 
-        //* Creating Dropdown
-        Center(
-          child: DropdownButton(
-            hint: Text(_currentOption),
-            //* List of items to convert
-            items: <String>[
-              "mil",
-              "pulgadas",
-              "Pie",
-              "Yarda",
-              "Rood",
-              "Cadena",
-              "Furlong",
-              "Milla",
-              "Legua",
-              "Link",
-              "Braza",
-            ].map<DropdownMenuItem<String>>(
-              (String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
+          //* Creating Dropdown
+          Center(
+            child: DropdownButton(
+              hint: Text(_currentOption),
+              //* List of items to convert
+              items: <String>[
+                "mil",
+                "pulgadas",
+                "Pie",
+                "Yarda",
+                "Rood",
+                "Cadena",
+                "Furlong",
+                "Milla",
+                "Legua",
+                "Link",
+                "Braza",
+              ].map<DropdownMenuItem<String>>(
+                (String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                },
+              ).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  _currentOption = newValue!;
+                });
               },
-            ).toList(),
-            onChanged: (String? newValue) {
-              setState(() {
-                _currentOption = newValue!;
-              });
-            },
+            ),
           ),
-        ),
-        RenderOption(
-          option: _currentOption,
-          value: _inputValue,
-        ),
-      ],
+          RenderOption(
+            option: _currentOption,
+            value: _inputValue,
+          ),
+        ],
+      ),
     );
   }
 }
