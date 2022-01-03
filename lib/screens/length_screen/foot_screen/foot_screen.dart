92ca33d9194a561d53fd81a250a6db27683e2aeb
chris.am <c.aranguizm@outlook.com>
2022-01-03 03:07:08 -0300

    adding foot info in screen

--- lib/screens/length_screen/foot_screen/foot_screen.dart
+++ lib/screens/length_screen/foot_screen/foot_screen.dart
@@ -13,6 +13,35 @@ class FootScreen extends StatelessWidget {
         children: [
           const Text('Pie'),
           FeetToMetric(feet: footInput),
+          Container(
+            padding: const EdgeInsets.only(top: 20),
+            width: 300,
+            child: Column(
+              children: [
+                const Text(
+                  'Equivalencia:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.5,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Column(
+                    children: const [
+                      Text('0.0000631313 leguas'),
+                      Text('0.0001893939 millas'),
+                      Text('0.333334 yardas'),
+                      Text('1.5151515152 links'),
+                      Text('0.3048006096 metros'),
+                      Text('3.048006096 decimetros'),
+                      Text('0.0003048006 kilometros'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
