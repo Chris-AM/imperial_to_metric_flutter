95fbd2e353f7b1c4b58f5266fc0394ac53e7296e
chris.am <c.aranguizm@outlook.com>
2022-01-03 03:11:49 -0300

    adding furlong info in screen

--- lib/screens/length_screen/furlong_screen/furlong_screen.dart
+++ lib/screens/length_screen/furlong_screen/furlong_screen.dart
@@ -13,6 +13,35 @@ class FurlongScreen extends StatelessWidget {
         children: [
           const Text('Furlong'),
           FurlongToMetric(furlong: furlongInput),
+          Container(
+            padding: const EdgeInsets.only(top: 20),
+            width: 300,
+            child: Column(
+              children: [
+                const Text(
+                  'Equivalencia:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.5,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Column(
+                    children: const [
+                      Text('0.0416665833 leguas'),
+                      Text('0.12499975millas'),
+                      Text('220 yardas'),
+                      Text('660 pies'),
+                      Text('201.168 metros'),
+                      Text('2011.68 decimetros'),
+                      Text('0.201168 kilometros'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
