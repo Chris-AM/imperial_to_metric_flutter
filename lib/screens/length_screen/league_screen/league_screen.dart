c16e2078cd7bce5129637f6e53b4f06fb9342a1c
chris.am <c.aranguizm@outlook.com>
2022-01-04 00:29:17 -0300

    adding leagues info in screen

--- lib/screens/length_screen/league_screen/league_screen.dart
+++ lib/screens/length_screen/league_screen/league_screen.dart
@@ -13,6 +13,35 @@ class LeagueScreen extends StatelessWidget {
         children: [
           const Text('Leaguas'),
           LeagueToMetric(league: leagueInput),
+          Container(
+            padding: const EdgeInsets.only(top: 20),
+            width: 300,
+            child: Column(
+              children: [
+                const Text(
+                  'Equivalencia:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.5,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Column(
+                    children: const [
+                      Text('190080 pulgadas'),
+                      Text('3 millas'),
+                      Text('5280 yardas'),
+                      Text('15840 pies'),
+                      Text('4828.032 metros'),
+                      Text('48280.32 decimetros'),
+                      Text('4.828032 kilometros'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
