db74887cad1e8080197675863c3115d804395649
chris.am <c.aranguizm@outlook.com>
2022-01-04 00:56:09 -0300

    adding yard info in screen

--- lib/screens/length_screen/yard_screen/yard_screen.dart
+++ lib/screens/length_screen/yard_screen/yard_screen.dart
@@ -13,6 +13,35 @@ class YardScreen extends StatelessWidget {
         children: [
           const Text('Yarda'),
           YardToMetric(yard: yardInput),
+          Container(
+            padding: const EdgeInsets.only(top: 20),
+            width: 300,
+            child: Column(
+              children: [
+                const Text(
+                  'Equivalencia:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.5,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Column(
+                    children: const [
+                      Text('0.0001893939 leguas'),
+                      Text('0.0005681818 millas'),
+                      Text('36 pulgadas'),
+                      Text('3 pies'),
+                      Text('0.9144 metros'),
+                      Text('9.144 decimetros'),
+                      Text('0.0009144 kilometros'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
