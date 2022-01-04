b42a2220c907210327343120b1a713254e1be766
chris.am <c.aranguizm@outlook.com>
2022-01-04 00:33:40 -0300

    adding links info in screen

--- lib/screens/length_screen/link_screen/link_screen.dart
+++ lib/screens/length_screen/link_screen/link_screen.dart
@@ -13,6 +13,35 @@ class LinkScreen extends StatelessWidget {
         children: [
           const Text('Links'),
           LinkToMetric(link: linkInput),
+          Container(
+            padding: const EdgeInsets.only(top: 20),
+            width: 300,
+            child: Column(
+              children: [
+                const Text(
+                  'Equivalencia:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.5,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Column(
+                    children: const [
+                      Text('0.0000416667 leguas'),
+                      Text('0.000125 millas'),
+                      Text('0.22 yardas'),
+                      Text('0.66 pies'),
+                      Text('0.201168 metros'),
+                      Text('2.01168 decimetros'),
+                      Text('0.000201168 kilometros'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
