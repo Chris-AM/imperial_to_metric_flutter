4a354ebf46abdf33827f57080fa19e4828f65bf5
chris.am <c.aranguizm@outlook.com>
2022-01-04 00:40:07 -0300

    adding mils info in screen

--- lib/screens/length_screen/mil_screen/mil_screen.dart
+++ lib/screens/length_screen/mil_screen/mil_screen.dart
@@ -11,8 +11,37 @@ class MilScreen extends StatelessWidget {
       padding: const EdgeInsets.only(top: 20),
       child: Column(
         children: [
-          const Text('Mil'),
+          const Text('Mil (Milésima de pulgada)'),
           MilToMetric(mil: milInput),
+          Container(
+            padding: const EdgeInsets.only(top: 20),
+            width: 300,
+            child: Column(
+              children: [
+                const Text(
+                  'Equivalencia:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.5,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Column(
+                    children: const [
+                      Text('0.001 pulgadas'),
+                      Text('25.4 micrómetros'),
+                      Text('0.0000012626 cadenas'),
+                      Text('0.0000833333 pies'),
+                      Text('0.0000254 metros'),
+                      Text('0.000254 decimetros'),
+                      Text('2.539999999e-8 kilometros'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
