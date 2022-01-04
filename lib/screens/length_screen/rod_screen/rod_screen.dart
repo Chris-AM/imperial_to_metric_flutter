46e3b5253a721e2397c139ed6c81ff1a3f8d5a34
chris.am <c.aranguizm@outlook.com>
2022-01-04 00:49:02 -0300

    adding rod info in screen

--- lib/screens/length_screen/rod_screen/rod_screen.dart
+++ lib/screens/length_screen/rod_screen/rod_screen.dart
@@ -8,13 +8,42 @@ class RodScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-          padding: const EdgeInsets.only(top: 20),
-          child: Column(
-            children: [
-              const Text('Rod'),
-              RodToMetric(rod: rodInput),
-            ],
+      padding: const EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          const Text('Rod'),
+          RodToMetric(rod: rodInput),
+          Container(
+            padding: const EdgeInsets.only(top: 20),
+            width: 300,
+            child: Column(
+              children: [
+                const Text(
+                  'Equivalencia:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.5,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Column(
+                    children: const [
+                      Text('0.0010416667 leguas'),
+                      Text('0.003125 millas'),
+                      Text('5.5 yardas'),
+                      Text('16.5 pies'),
+                      Text('5.0292 metros'),
+                      Text('50.292 decimetros'),
+                      Text('0.0050292 kilometros'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        );
+        ],
+      ),
+    );
   }
 }
